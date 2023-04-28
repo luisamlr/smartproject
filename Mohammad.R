6bec54036a9b96578d5d21b3c7e41b27eb624dcd
Mohammad <motaromi@outlook.com>
2023-04-28 03:10:36 +0200

    Some minor changes

--- Mohammad.R
+++ Mohammad.R
@@ -5,3 +5,15 @@ library(jsonlite)
 library(googleway)
 
 key <- Sys.getenv("MY_API_KEY")
+
+location <- "50.8514%2C5.6900"
+radius <- 1000
+keyword <- "charging"
+url <- paste0("https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
+              "location=", location,
+              "&radius=", radius,
+              "&keyword=", keyword,
+              "&key=", key)
+response <- GET(url)
+places <- fromJSON(rawToChar(response$content))
+results <- places$results
