149c9b91402efcc27bd7b395f1505c8e1f6761f0
datacrafter <rogerpf1422@gmail.com>
2023-05-04 20:51:30 +0200

    I have improve the code so it properly counts for the closest stations, before it was only considering the 2 closest and itself. Furthermore i have performed some data cleaning. I have also computed how many charging station are in the sorrunding area, within 100, 250, 500 and 1000 meters. There is not much relations with the rating, yet there is clearly a relation with the province.
    Further analysis should be performed, and also consider more variables. Next step should try to assess the way opening hours affect the review.

--- Data Analysis.R
+++ Data Analysis.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(ggplot2)
+library(geosphere)
 
 #### Data Analysis####
 
@@ -31,44 +32,52 @@ df_cs_loc<-df_cs%>%
 # plot the data with rotated x-axis labels
 ggplot(df_cs, aes(x = Admin.Area.Level.1, y = Rating)) +
   geom_boxplot(fill = grey(0.8), color = "black", alpha = 0.7) +
-  labs(title = "Distribution of Reviews by Location",
+  labs(title = "Distribution of Reviews by Province",
        x = "Location",
        y = "Review") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
-library(geosphere)
-
 # function to calculate distance between two coordinates using geosphere package
 calc_distance <- function(lat1, lon1, lat2, lon2) {
-  dist <- distHaversine(c(lon1, lat1), c(lon2, lat2))/1000000# convert to km
+  dist <- distHaversine(c(lon1, lat1), c(lon2, lat2))
   return(dist)
 }
 
-# function to find 3 nearest charging stations based on distance
-find_3_nearest <- function(lat, lon, df_cs) {
-  df_cs$Distance <- sapply(1:nrow(df_cs), function(i) calc_distance(lat, lon, df_cs$Latitude[i], df_cs$Longitude[i]))
+# function to find the nearest charging stations based on distance
+find_nearest <- function(lat, lon, df_cs, closest = 1) {
+  df_cs$Distance <- sapply(1:nrow(df_cs), function(i) {
+    if (df_cs$Latitude[i] == lat && df_cs$Longitude[i] == lon) {
+      return(Inf) # set distance to infinity for charging station at given location
+    } else {
+      return(calc_distance(lat, lon, df_cs$Latitude[i], df_cs$Longitude[i]))
+    }
+  })
   df_sorted <- df_cs[order(df_cs$Distance),]
-  return(head(df_sorted, n=3))
+  return(head(df_sorted, n=closest))
 }
 
-# function to compute average rating of the 3 nearest charging stations
-avg_rating_3_nearest <- function(lat, lon, df) {
-  nearest_stations <- find_3_nearest(lat, lon, df)
+# function to compute average rating of the nearest charging stations
+rating_nearest <- function(lat, lon, df, closest_cs = 1) {
+  nearest_stations <- find_nearest(lat, lon, df,closest_cs)
   avg_rating <- mean(nearest_stations$Rating)
   return(avg_rating)
 }
 
-# test the find_3_nearest function for Station1
-find_3_nearest(df_cs$Latitude[1], df_cs$Longitude[1], df_cs)
-# test the avg_rating_3_nearest function for Station1
-avg_rating_3_nearest(df_cs$Latitude[1], df_cs$Longitude[1], df_cs)
-
+n_rating <- c()
 avg3 <- c()
+avg5 <- c()
+avg10 <- c()
+nrow(df_cs)
 for (i in 1:nrow(df_cs)){
-  # test the avg_rating_3_nearest function for Station1
-  avg3[i]<-avg_rating_3_nearest(df_cs$Latitude[i], df_cs$Longitude[i], df_cs)
+  # Find the nearest rating per CS
+  n_rating[i]<-rating_nearest(df_cs$Latitude[i], df_cs$Longitude[i], df_cs,1)
+  # Find the avg 3 nearest rating per CS
+  avg3[i]<-rating_nearest(df_cs$Latitude[i], df_cs$Longitude[i], df_cs, 3)
+  # Find the avg 5 nearest rating per CS
+  avg5[i]<-rating_nearest(df_cs$Latitude[i], df_cs$Longitude[i], df_cs, 5) 
+  # Find the avg 10 nearest rating per CS
+  avg10[i]<-rating_nearest(df_cs$Latitude[i], df_cs$Longitude[i], df_cs, 10)
 }
 
 
@@ -87,7 +96,7 @@ for (i in 1:nrow(df_cs)){
 
 #### Calculate how many stations are under 1000m, 500m and 200m
 
-# Import the dataset
+# Import the dataset ---- We work with the whole dataset since we do not care whether they have review or not in order to take into account 
 df_all<-read.csv("All_Chargers.csv")
 df_all<-df_all[df_all$Country=="Netherlands" ,]
 
@@ -95,27 +104,158 @@ df_all<-df_all[df_all$Country=="Netherlands" ,]
 counts_1000 <- c()
 counts_500 <- c()
 counts_250 <- c()
+counts_100<-c()
 # loop over each row in the dataframe
 
 for (i in 1:nrow(df_all)) {
   # check if the row has a rating
   if ((df_all$Ratings.Total[i] > 0)) {
-    # calculate distance between this row and all other rows
-    distances <- apply(df_all[, c("Latitude", "Longitude")], 1, 
-                       function(x) calc_distance(df_all$Latitude[i], df_all$Longitude[i], x[1], x[2]))
+  # check if the row has a rating
+    distances<-c()
+  # calculate distance between this row and all other rows
+    for (j in 1:nrow(df_all)){
+      distances[j]<- distHaversine(c(df_all$Longitude[i], df_all$Latitude[i]), c(df_all$Longitude[j], df_all$Latitude[j]))
+    }
     # count how many distances are less than 1000m
-    count_1000 <- length(df_all[distances < 1000,])
+    count_1000 <- nrow(df_all[distances < 1000,]) - 1 #Subtract 1 because otherwise we count the station itself
     # count how many distances are less than 500m
-    count_500 <- length(df_all[distances < 500,])
+    count_500 <- nrow(df_all[distances < 500,])  - 1 #Subtract 1 because otherwise we count the station itself
     # count how many distances are less than 250m
-    count_250 <- length(df_all[distances < 250,])
+    count_250 <- nrow(df_all[distances < 250,])  - 1 #Subtract 1 because otherwise we count the station itself
+    # count how many distances are less than 100m
+    count_100 <- nrow(df_all[distances < 100,])  - 1 #Subtract 1 because otherwise we count the station itself
     # add count to vector
     counts_1000<- c(counts_1000, count_1000)
     # add count to vector
     counts_500<- c(counts_500, count_500)
     # add count to vector
     counts_250<- c(counts_250, count_250)
+    # add count to vector
+    counts_100<- c(counts_100, count_100)
   }
 }
+
+
+# Merging with previous information
+df_cs<-cbind(df_cs, n_rating, avg3,avg5, avg10, counts_100, counts_250, counts_500, counts_1000)
+
+##### Relation 1 closest locations with the star rating of the charging station #####
+ggplot(df_cs, aes(x = Rating, y = n_rating)) +
+  geom_point(color = "black") +
+  geom_smooth(method = "lm", color = "red") +
+  xlim(1, 5) + ylim(1, 5) +
+  labs(x = "Star Rating of Charging Station", 
+       y = "Star Rating of the Closest Locations", 
+       title = "Relationship Between Charging Station Rating and the Rating of the Closest Charging Station")
+
+##### Relation 3 closest locations with the star rating of the charging station #####
+ggplot(df_cs, aes(x = Rating, y = avg3)) +
+  geom_point(color = "black") +
+  geom_smooth(method = "lm", color = "red") +
+  xlim(1, 5) + ylim(1, 5) +
+  labs(x = "Star Rating of Charging Station", 
+       y = "Average Star Rating of 3 Closest Locations", 
+       title = "Relationship Between Charging Station Rating and Average Rating of 3 Closest Charging Stations")
+
+##### Relation 5 closest locations with the star rating of the charging station #####
+ggplot(df_cs, aes(x = Rating, y = avg5)) +
+  geom_point(color = "black") +
+  geom_smooth(method = "lm", color = "red") +
+  xlim(1, 5) + ylim(1, 5) +
+  labs(x = "Star Rating of Charging Station", 
+       y = "Average Star Rating of 5 Closest Locations", 
+       title = "Relationship Between Charging Station Rating and Average Rating of 5 Closest Charging Stations")
+
+##### Relation 10 closest locations with the star rating of the charging station #####
+ggplot(df_cs, aes(x = Rating, y = avg10)) +
+  geom_point(color = "black") +
+  geom_smooth(method = "lm", color = "red") +
+  xlim(1, 5) + ylim(1, 5) +
+  labs(x = "Star Rating of Charging Station", 
+       y = "Average Star Rating of 10 Closest Locations", 
+       title = "Relationship Between Charging Station Rating and Average Rating of 10 Closest Charging Stations")
+
+##### Charging Stations in the sorrounding area 100 #####
+# plot the data with rotated x-axis labels
+ggplot(df_cs, aes(x = Admin.Area.Level.1, y = counts_100)) +
+  geom_boxplot(fill = grey(0.8), color = "black", alpha = 0.7) +
+  labs(title = "Charging Station within 100m by Province",
+       x = "Province",
+       y = "Charging Stations") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+##### Charging Stations in the sorrounding area 250 #####
+# plot the data with rotated x-axis labels
+ggplot(df_cs, aes(x = Admin.Area.Level.1, y = counts_250)) +
+  geom_boxplot(fill = grey(0.8), color = "black", alpha = 0.7) +
+  labs(title = "Charging Station within 250m by Province",
+       x = "Province",
+       y = "Charging Stations") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+##### Charging Stations in the sorrounding area 500 #####
+# plot the data with rotated x-axis labels
+ggplot(df_cs, aes(x = Admin.Area.Level.1, y = counts_500)) +
+  geom_boxplot(fill = grey(0.8), color = "black", alpha = 0.7) +
+  labs(title = "Charging Station within 500m by Province",
+       x = "Province",
+       y = "Charging Stations") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+##### Charging Stations in the sorrounding area 1000 #####
+# plot the data with rotated x-axis labels
+ggplot(df_cs, aes(x = Admin.Area.Level.1, y = counts_1000)) +
+  geom_boxplot(fill = grey(0.8), color = "black", alpha = 0.7) +
+  labs(title = "Charging Station within 1000m by Province",
+       x = "Province",
+       y = "Charging Stations") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+##### Charging Stations in the sorrounding area 100  - Rating#####
+# plot the data with rotated x-axis labels
+ggplot(df_cs, aes(x = as.factor(round(Rating,0)), y = counts_100)) +
+  geom_boxplot(fill = grey(0.8), color = "black", alpha = 0.7) +
+  labs(title = "Charging Station within 100m by Rating",
+       x = "Review",
+       y = "Charging Stations") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+##### Charging Stations in the sorrounding area 250  - Rating#####
+# plot the data with rotated x-axis labels
+ggplot(df_cs, aes(x = as.factor(round(Rating,0)), y = counts_250)) +
+  geom_boxplot(fill = grey(0.8), color = "black", alpha = 0.7) +
+  labs(title = "Charging Station within 250m by Rating",
+       x = "Review",
+       y = "Charging Stations") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+##### Charging Stations in the sorrounding area 500  - Rating#####
+# plot the data with rotated x-axis labels
+ggplot(df_cs, aes(x = as.factor(round(Rating,0)), y = counts_500)) +
+  geom_boxplot(fill = grey(0.8), color = "black", alpha = 0.7) +
+  labs(title = "Charging Station within 250m by Rating",
+       x = "Review",
+       y = "Charging Stations") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+##### Charging Stations in the sorrounding area 1000  - Rating#####
+# plot the data with rotated x-axis labels
+ggplot(df_cs, aes(x = as.factor(round(Rating,0)), y = counts_1000)) +
+  geom_boxplot(fill = grey(0.8), color = "black", alpha = 0.7) +
+  labs(title = "Charging Station within 250m by Rating",
+       x = "Review",
+       y = "Charging Stations") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
 
 
