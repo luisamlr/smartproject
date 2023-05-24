111a2c09a6dd73acb69ab92d1bb8d456e302e4f6
Luisa <115930123+luisamlr@users.noreply.github.com>
2023-05-24 10:14:39 +0200

    Delete variable_control.R

--- variable_control.R
+++
@@ -1,8 +0,0 @@
-################################
-# Smart Project Data Cleaning #
-###############################
-
-
-reshaped_table <- read.csv("/Users/isamahler/Desktop/SmartRProject/reshaped_table.csv", header = TRUE, sep = ",")
-combined_data <- read.csv("/Users/isamahler/Desktop/SmartRProject/combined_data.csv")
-
